92aeca37a2b026192e7cbc39902c69f8f09557cc
naveen9036 <naveen903612@gmail.com>
2020-04-25 17:28:28 +0530

    updated

--- UserHome/views.py
+++ UserHome/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    context = {'note': 'Hello World'
+
+               }
+    return render(request, "UserHome/index.html", context)
